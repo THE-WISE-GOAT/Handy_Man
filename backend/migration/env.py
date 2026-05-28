fcb34062dbd96cbe7d8049dd45154405e3bb5258
Sushant553546 <raut.sushant2005@gmail.com>
2026-05-28 08:18:39 +0545

    Added .env

--- backend/migration/env.py
+++ backend/migration/env.py
@@ -1,19 +1,13 @@
-
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
+
 from alembic import context
-from src.core.model import Base
-from src.configuration.config import settings
-from geoalchemy2 import alembic_helpers
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-database_url = f"postgresql://{settings.DATABASE_USERNAME}:{settings.DATABASE_PASSWORD}@{settings.DATABASE_HOSTNAME}:{settings.DATABASE_PORT}/{settings.DATABASE_NAME}"
-
-config.set_main_option("sqlalchemy.url", database_url)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -24,7 +18,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = Base.metadata
+target_metadata = None
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -44,17 +38,12 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    def run_migrations_offline() -> None:
-        url = config.get_main_option("sqlalchemy.url")
-        context.configure(
+    url = config.get_main_option("sqlalchemy.url")
+    context.configure(
         url=url,
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
-        compare_type=True,  # <-- ADD THIS
-        include_object=alembic_helpers.include_object,  # <-- ADD THIS
-        process_revision_directives=alembic_helpers.writer,  # <-- ADD THIS
-        render_item=alembic_helpers.render_item,  # <-- ADD THIS
     )
 
     with context.begin_transaction():
@@ -62,6 +51,12 @@ def run_migrations_offline() -> None:
 
 
 def run_migrations_online() -> None:
+    """Run migrations in 'online' mode.
+
+    In this scenario we need to create an Engine
+    and associate a connection with the context.
+
+    """
     connectable = engine_from_config(
         config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",
@@ -70,12 +65,7 @@ def run_migrations_online() -> None:
 
     with connectable.connect() as connection:
         context.configure(
-            connection=connection,
-            target_metadata=target_metadata,
-            compare_type=True,  # <-- ADD THIS
-            include_object=alembic_helpers.include_object,  # <-- ADD THIS
-            process_revision_directives=alembic_helpers.writer,  # <-- ADD THIS
-            render_item=alembic_helpers.render_item,  # <-- ADD THIS
+            connection=connection, target_metadata=target_metadata
         )
 
         with context.begin_transaction():
--- backend/src/configuration/config.py
+++ backend/src/configuration/config.py
@@ -2,12 +2,12 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 from pathlib import Path
 
 
-BASE_DIR = Path(__file__).resolve().parents[3] #this gets the path to the root folder (Handy_Man). This moves up 3 levels from config.py to hit .env
-# config.py -> configuration -> src -> backend-> root
+BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent #this gets the path to the root folder (Handy_Man). This moves up 3 levels from config.py to hit .env
+# config.py -> configuration -> src -> root 
 
-ENV_PATH = str(BASE_DIR / ".env") # this stores the path to the .env file in a variable so we can use it in our Settings class
+ENV_PATH = BASE_DIR / ".env" # this stores the path to the .env file in a variable so we can use it in our Settings class
 
-print(f"--- Alembic Debug: Looking for .env file at: {ENV_PATH} ---")
+
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=ENV_PATH) # specify the .env file to import all the environment variables and load them
     
@@ -21,8 +21,6 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str   # this is the algorithm used to sign our JWT tokens, HS256 is a common choice for symmetric signing
     ACCESS_TOKEN_EXPIRE_MINUTES: int # this is the number of minutes that our access tokens will be valid for, after that they will expire and the user will need to log in again to get a new token
-    gemini_api_key: str
-    
     
 settings = Settings() # creates an instance of the Settings class, which will read the environment variables from the .env file and make them available when ever needed 
 
