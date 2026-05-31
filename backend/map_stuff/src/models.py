d193ead44bbcced5613a8ea85a7de861169a5b51
p4kri <prajanadhikari786.com>
2026-05-31 22:44:09 +0545

    chore: setup alembic migrations and define async postgres database models

---
+++ backend/map_stuff/src/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, ARRAY
+from geoalchemy2 import Geography
+from src.database import Base
+
+class Worker(Base):
+    __tablename__ = "workers"
+
+    user_id = Column(Integer, primary_key=True, index=True)
+    radius = Column(Integer, nullable=False)
+    tags = Column(ARRAY(String), nullable=False)
+    # Native PostGIS geography column tracking 2D space (SRID 4326)
+    location = Column(Geography(geometry_type='POINT', srid=4326, spatial_index=True), nullable=False)
