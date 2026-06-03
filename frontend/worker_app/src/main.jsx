508b4aeb56963d1d14bb7555ee17a127270d2304
Anup Guragain <anupguragain123@gmail.com>
2026-06-03 19:42:48 +0545

    feat: add worker/client mode switching buttons with role-based visibility

--- frontend/worker_app/src/main.jsx
+++ frontend/worker_app/src/main.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx'
-// This links the exact same beautiful styles you just wrote to the worker app
-import '../../shared/styles/global.css' 
+import { AuthProvider } from '@shared/context/AuthContext';
+import '../../shared/styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
