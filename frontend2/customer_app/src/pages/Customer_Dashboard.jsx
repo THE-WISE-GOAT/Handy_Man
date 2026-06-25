07424f15061282b8dec1cd1aea8e57209ebfdfde
Anup Guragain <anupguragain123@gmail.com>
2026-06-25 19:16:31 +0545

    new frontend

--- frontend2/customer_app/src/pages/Customer_Dashboard.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import CustomerDashboardView from "../components/customer-dashboard/CustomerDashboardView";
-
-export default function CustomerDashboard(props) {
-  return <CustomerDashboardView {...props} />;
-}
